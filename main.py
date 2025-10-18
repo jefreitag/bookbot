00288eb0901714071dce4f74b4cac2c58a5b7b56
jefreitag <jfreitag152@gmail.com>
2025-10-18 18:43:12 -0500

    rename find_word_num to get_num_words and move it to stats.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
+from stats import get_num_words
+
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
         
     return file_contents
 
-def find_word_num(text):
-    return len(text.split())
-
 def main():
-    word_num = find_word_num(get_book_text('./books/frankenstein.txt'))
+    word_num = get_num_words(get_book_text('./books/frankenstein.txt'))
     print(f"Found {word_num} total words")
 
 main()
