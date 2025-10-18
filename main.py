5c5fe2e5fe1313752f640dc95476fad7cb26f115
jefreitag <jfreitag152@gmail.com>
2025-10-18 18:39:04 -0500

    add find_word_num method and print word num from main

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ def get_book_text(file_path):
         
     return file_contents
 
+def find_word_num(text):
+    return len(text.split())
+
 def main():
-    print(get_book_text('./books/frankenstein.txt'))
+    word_num = find_word_num(get_book_text('./books/frankenstein.txt'))
+    print(f"Found {word_num} total words")
 
 main()
